53222d58f0e2f44846050248a703bb4da6fc5e47
Jordan Jacobson <jordandurjacobson@gmail.com>
2022-01-22 14:00:04 +1100

    Added game function which takes all functions so far, plays the RPS game 5 times, logs the results of each game in the console and then logs the winner at the end.

--- rps.js
+++ rps.js
@@ -1,27 +1,37 @@
-let playerSelection = 'scissors';
-let computerSelection = computerPlay();
+let playerScore = 0;
+let computerScore = 0;
 
+function game() {
+    for (let i= 0; i < 6; i++){
+        let playerSelection = prompt("paper, scissors, rock?");
+        let computerSelection = computerPlay();
 
-function computerPlay (){
-    const options = ["rock", "paper", "scissors"];
-
-    computerOutput = (Math.floor(Math.random() * options.length));
-    return (computerOutput, options[computerOutput]);
+        computerPlay();
+        console.log(round(computerSelection, playerSelection));
+    }
+    console.log(winner(playerScore, computerScore));
 }
+
 
 function round (computerSelection, playerSelection){
     computerPlay();
     if (playerSelection === 'scissors' && computerSelection === 'paper'){
+        playerScore++;
         return ('Scissors beats paper. Player won!');
     } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+        playerScore++;
         return ('Paper beats rock. Player won!');
     } else if (playerSelection === 'rock' && computerSelection ==='scissors'){
+        playerScore++;
         return ('Rock beats scissors. Player won!');
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore++;
         return ('Scissors beats paper. Computer won!')
     } else if (playerSelection === 'rock' && computerSelection === 'paper'){
+        computerScore++;
         return ('Paper beats rock. Computer won!');
     } else if (playerSelection === 'scissors' && computerSelection ==='rock'){
+        computerScore++;
         return ('Rock beats scissors. Computer won!');
     } else if (playerSelection === computerSelection){
         return ("It's a draw!");
@@ -30,4 +40,16 @@ function round (computerSelection, playerSelection){
     }
 }
 
-console.log(round(computerSelection, playerSelection));
+function computerPlay (){
+    const options = ["rock", "paper", "scissors"];
+    computerOutput = (Math.floor(Math.random() * options.length));
+    return (computerOutput, options[computerOutput]);
+}
+
+function winner (playerScore, computerScore){
+    if (playerScore > computerScore){
+        return ("Player won! Score:" + playerScore + "-" + computerScore);
+    } else {
+        return ("Computer won! Score:" + computerScore + "-" + playerScore);
+    }
+}
