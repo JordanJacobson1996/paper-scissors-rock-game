e7643a9ef8b6a5dfaaf37bf757da6abfa69c6363
Jordan Jacobson <jordandurjacobson@gmail.com>
2022-02-02 08:23:40 +1100

    Updated code so that when the button is clicked it returns the value of the button as the playerSelection variable

--- rps.js
+++ rps.js
@@ -1,45 +1,46 @@
 let playerScore = 0;
 let computerScore = 0;
+let playerSelection;
 
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach(button => {
-    button.addEventListener('click', round);
-});
-
-function game() {
-    for (let i= 0; i < 5; i++){
-        let playerSelection = prompt("paper, scissors, rock?");
-        let computerSelection = computerPlay();
-
-        computerPlay();
-        console.log(round(computerSelection, playerSelection));
-    }
-    console.log(winner(playerScore, computerScore));
+document.getElementById('Scissors').onclick = function (e){
+    return playerSelection = 'scissors';
 }
 
+document.getElementById('Paper').onclick = function (e){
+    return playerSelection = 'paper';
+}
 
-function round (computerSelection, playerSelection){
-    computerPlay();
+document.getElementById('Rock').onclick = function (e){
+    return playerSelection = 'rock';
+}
+
+function round (playerSelection, computerSelection){
     if (playerSelection === 'scissors' && computerSelection === 'paper'){
+        console.log(playerSelection, computerSelection);
         playerScore++;
         return ('Scissors beats paper. Player won!');
     } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+        console.log(playerSelection, computerSelection);
         playerScore++;
         return ('Paper beats rock. Player won!');
     } else if (playerSelection === 'rock' && computerSelection ==='scissors'){
+        console.log(playerSelection, computerSelection);
         playerScore++;
         return ('Rock beats scissors. Player won!');
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        console.log(playerSelection, computerSelection);
         computerScore++;
         return ('Scissors beats paper. Computer won!')
     } else if (playerSelection === 'rock' && computerSelection === 'paper'){
+        console.log(playerSelection, computerSelection);
         computerScore++;
         return ('Paper beats rock. Computer won!');
     } else if (playerSelection === 'scissors' && computerSelection ==='rock'){
+        console.log(playerSelection, computerSelection);
         computerScore++;
         return ('Rock beats scissors. Computer won!');
     } else if (playerSelection === computerSelection){
+        console.log(playerSelection, computerSelection);
         return ("It's a draw!");
     } else {
         return null;
