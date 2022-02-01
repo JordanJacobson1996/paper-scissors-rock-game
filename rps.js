224242e2863fa5a57e3ef27c2cdb7a009e232801
Jordan Jacobson <jordandurjacobson@gmail.com>
2022-02-02 09:51:41 +1100

    Change the buttons so that when clicked they play a round of rps and update the score

--- rps.js
+++ rps.js
@@ -1,46 +1,45 @@
 let playerScore = 0;
 let computerScore = 0;
-let playerSelection;
 
-document.getElementById('Scissors').onclick = function (e){
-    return playerSelection = 'scissors';
+document.getElementById('Scissors').onclick = function(){
+    let playerSelection = this.id;
+    round(playerSelection);
 }
 
-document.getElementById('Paper').onclick = function (e){
-    return playerSelection = 'paper';
+document.getElementById('Paper').onclick = function(){
+    let playerSelection = this.id;
+    round(playerSelection);
 }
 
-document.getElementById('Rock').onclick = function (e){
-    return playerSelection = 'rock';
-}
+document.getElementById('Rock').onclick = function(){
+    let playerSelection = this.id;
+    round(playerSelection);
+} 
 
-function round (playerSelection, computerSelection){
-    if (playerSelection === 'scissors' && computerSelection === 'paper'){
-        console.log(playerSelection, computerSelection);
+
+function round (playerSelection){
+
+    let computerSelection = computerPlay();
+
+    if (playerSelection === 'Scissors' && computerSelection === 'paper'){
         playerScore++;
         return ('Scissors beats paper. Player won!');
-    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
-        console.log(playerSelection, computerSelection);
+    } else if (playerSelection === 'Paper' && computerSelection === 'rock'){
         playerScore++;
         return ('Paper beats rock. Player won!');
-    } else if (playerSelection === 'rock' && computerSelection ==='scissors'){
-        console.log(playerSelection, computerSelection);
+    } else if (playerSelection === 'Rock' && computerSelection ==='scissors'){
         playerScore++;
         return ('Rock beats scissors. Player won!');
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        console.log(playerSelection, computerSelection);
+    } else if (playerSelection === 'Paper' && computerSelection === 'scissors') {
         computerScore++;
         return ('Scissors beats paper. Computer won!')
-    } else if (playerSelection === 'rock' && computerSelection === 'paper'){
-        console.log(playerSelection, computerSelection);
+    } else if (playerSelection === 'Rock' && computerSelection === 'paper'){
         computerScore++;
         return ('Paper beats rock. Computer won!');
-    } else if (playerSelection === 'scissors' && computerSelection ==='rock'){
-        console.log(playerSelection, computerSelection);
+    } else if (playerSelection === 'Scissors' && computerSelection ==='rock'){
         computerScore++;
         return ('Rock beats scissors. Computer won!');
     } else if (playerSelection === computerSelection){
-        console.log(playerSelection, computerSelection);
         return ("It's a draw!");
     } else {
         return null;
