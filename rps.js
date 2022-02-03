30271c1e9981f71e89bbc2a4bbb49bf5f3e7ed00
Jordan Jacobson <jordandurjacobson@gmail.com>
2022-02-03 13:29:39 +1100

    Completed product. Updated the HTML and added CSS to make it pretty. Finally completed the JS code so that it does not require a final click after one of the players has reached a score of five.

--- rps.js
+++ rps.js
@@ -1,55 +1,65 @@
 let playerScore = 0;
 let computerScore = 0;
 
-displayScore(playerScore, computerScore);
+displayScore(playerScore, computerScore); // displays score of zero at start of game
 
 document.getElementById('scissors').onclick = function(){
-    let playerSelection = this.id;
+    //sets playerSelection to the id of the button pressed
+    let playerSelection = this.id; 
     round(playerSelection);
+    game(playerScore, computerScore);
 }
 
 document.getElementById('paper').onclick = function(){
     let playerSelection = this.id;
     round(playerSelection);
+    game(playerScore, computerScore);
 }
 
 document.getElementById('rock').onclick = function(){
     let playerSelection = this.id;
     round(playerSelection);
-} 
-
-
-function round (playerSelection){
-
-    let computerSelection = computerPlay();
-    let resultContent;
-
-    if (playerSelection === 'scissors' && computerSelection === 'paper'){
-        playerScore++;
-        resultContent = 'Scissors beats paper. Player won!';
-    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
-        playerScore++;
-        resultContent = 'Paper beats rock. Player won!';
-    } else if (playerSelection === 'rock' && computerSelection ==='scissors'){
-        playerScore++;
-        resultContent = 'Rock beats scissors. Player won!';
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        computerScore++;
-        resultContent = 'Scissors beats paper. Computer won!';
-    } else if (playerSelection === 'rock' && computerSelection === 'paper'){
-        computerScore++;
-        resultContent = 'Paper beats rock. Computer won!';
-    } else if (playerSelection === 'scissors' && computerSelection ==='rock'){
-        computerScore++;
-        resultContent = 'Rock beats Scissors. Computer won!';
-    } else if (playerSelection === computerSelection){
-        resultContent = "It's a draw!";
-    } else {
-        return null;
-    }
-    displayRoundResult(resultContent);
-    displayScore(playerScore, computerScore);
+    game(playerScore, computerScore);
 }
+
+//Checks if one of the players has reached the end of the game and displays the winner.
+function game (playerScore, computerScore){
+    if (playerScore === 5 || computerScore === 5){
+        displayScore(playerScore, computerScore);
+        winner(playerScore, computerScore);
+    }
+}
+
+//plays one round of the game, checks computer selection against player and outputs the winner.
+function round (playerSelection){ 
+        let computerSelection = computerPlay();
+        let resultContent;
+        if (playerSelection === 'scissors' && computerSelection === 'paper'){
+            playerScore++;
+            resultContent = 'Scissors beats paper. Player won!';
+        } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+            playerScore++;
+            resultContent = 'Paper beats rock. Player won!';
+        } else if (playerSelection === 'rock' && computerSelection ==='scissors'){
+            playerScore++;
+            resultContent = 'Rock beats scissors. Player won!';
+        } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+            computerScore++;
+            resultContent = 'Scissors beats paper. Computer won!';
+        } else if (playerSelection === 'rock' && computerSelection === 'paper'){
+            computerScore++;
+            resultContent = 'Paper beats rock. Computer won!';
+        } else if (playerSelection === 'scissors' && computerSelection ==='rock'){
+            computerScore++;
+            resultContent = 'Rock beats Scissors. Computer won!';
+        } else if (playerSelection === computerSelection){
+            resultContent = "It's a draw!";
+        } else {
+            return null;
+        }
+        displayRoundResult(resultContent);
+        displayScore(playerScore, computerScore);
+    }
 
 
 
@@ -61,11 +71,13 @@ function computerPlay (){
 
 function winner (playerScore, computerScore){
     if (playerScore > computerScore){
-        return ("Player won! Score:" + playerScore + "-" + computerScore);
-    } else if (computerScore > playerScore){
-        return ("Computer won! Score:" + computerScore + "-" + playerScore);
+            if (confirm("Player Won! Would you like to play again?")){
+                location.reload();
+            }
     } else {
-        return ("Its a draw!")
+            if (confirm("Computer Won! Would you like to play again?")){
+                location.reload();
+            }
     }
 }
 
@@ -74,6 +86,6 @@ function displayRoundResult (resultContent) {
 }
 
 function displayScore (playerScore, computerScore){
-    document.getElementById("pScore").textContent = "Player: " + playerScore;
-    document.getElementById("cScore").textContent = "Computer: " + computerScore;
+    document.getElementById("pScore").textContent = "Player: " + (playerScore);
+    document.getElementById("cScore").textContent = "Computer: " + (computerScore);
 }
